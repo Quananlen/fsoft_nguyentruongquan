b858a1e9dd91aaa5ccfcc9994895b7b7da546988
Quananlen <67568635+Quananlen@users.noreply.github.com>
2022-06-21 08:13:29 +0700

    Update AgeComparator.java

--- module2/src/unit12_java_colectiton_framework/practice/AgeComparator.java
+++ module2/src/unit12_java_colectiton_framework/practice/AgeComparator.java
@@ -1,4 +1,4 @@
-package unit12_java_colectiton_framework.practice;
+cotpackage unit12_java_colectiton_framework.practice;
 
 import java.util.Comparator;
 
@@ -6,8 +6,6 @@ public class AgeComparator implements Comparator<Student> {
     public int compare(Student student1, Student student2) {
         if(student1.getAge() > student2.getAge()){
             return 1;
-        }else if(student1.getAge() == student2.getAge()){
-            return 0;
         }else{
             return -1;
         }
